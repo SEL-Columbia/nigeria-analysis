410656f9fd0bff45ef9bf6d27b0f84eaabf8a800
modilabs-starscream <modilabs.starscream@gmail.com>
2013-02-14 17:51:07 -0500

    removing old code; adding ability to run with different cl arguments

--- benchmark.py
+++ benchmark.py
@@ -7,34 +7,8 @@ from pybamboo.dataset import Dataset
 from pybamboo.connection import Connection
 from pybamboo.exceptions import PyBambooException
 
-def print_time(func):
-    """
-    @print_time
-
-    Put this decorator around a function to see how many seconds each
-    call of this function takes to run.
-    """
-    def wrapped_func(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        seconds = end - start
-        outfile.write("SECONDS: %s\n" % seconds)
-        return result
-    return wrapped_func
 
 URL = 'http://localhost:8080'
-conn = Connection(url=URL)
-DIR = 'csvs/'
-# choosing between 100, 1000, 10000, 100000
-TESTING = 100
-curr_time = time.strftime('%Y-%m-%d-%H-%M-%S')
-hostname = os.uname()[1]
-
-# test on upload
-test_str = str(TESTING)
-water_file = DIR + test_str + '/water.csv'
-education_file = DIR + test_str + '/education.csv'
 
 def run_test_suite(dataset_file_path_list):
     alldata = []
@@ -130,37 +104,32 @@ def time_till_import_is_finished(dataset):
     after = time.time()
     return after - before
 
-@print_time
-def add_calculation(connection, dataset, name, formula):
-    pass
-
-
-@print_time
-def create_dataset(connection, path):
-    dataset = Dataset(connection=connection, path=path)
-    state = dataset.get_info()['state']
-    outfile.write('dataset created: %s\n' % dataset.id)
-    outfile.write('initial state: %s\n' % state)
-    while state == 'pending':
-        time.sleep(1)
-        state = dataset.get_info()['state']
-    outfile.write('final state: %s\n' % state)
-    outfile.write('finished importing dataset: %s\n' % dataset.id)
-    return dataset
-
-def _run_with(connection, data_file_list):
-    for data_file in data_file_list:
-        try:
-            dataset =create_dataset(connection=connection, path=data_file)
-            dataset.delete()
-        except PyBambooException as e:
-            outfile.write('pybamboo error: %s\n' % e)
-        time.sleep(1)
-
-#_run_with(conn, [water_file, education_file])
-#write_to_csv(run_test_suite([water_file, education_file]) )
 if __name__ == "__main__":
-    outfile = open('logs/%s-%s-%s.log' % (hostname, TESTING, curr_time), 'wb')
-    write_to_csv(run_test_suite([water_file]), outfile)
+    valid_test_sizes = ['100', '1000', '10000', '100000']
+    test_sizes = sys.argv[1:]
+    invalid_test_size = len(test_sizes) == 0 or len([x for x in test_sizes\
+            if x not in valid_test_sizes]) > 1
+
+    # choosing between 100, 1000, 10000, 100000
+    if invalid_test_size: 
+        print """provide size of datasets, like one of:
+                   python benchmark.py 1000
+                   python benchmark.py 100 1000 10000
+                Allowed test sizes are 10 100 1000 10000 and 100000:
+             """
+        sys.exit()
+
+    DIR = 'csvs/'
+    # test on upload
+    curr_time = time.strftime('%Y-%m-%d-%H-%M-%S')
+    hostname = os.uname()[1]
+    outfile = open('logs/%s-%s.log' % (hostname, curr_time), 'wb')
+    for test_size in test_sizes:
+        test_str = str(test_size)
+        water_file = DIR + test_size + '/water.csv'
+        education_file = DIR + test_size + '/education.csv'
+
+        #write_to_csv(run_test_suite([water_file, education_file]) )
+        write_to_csv(run_test_suite([water_file]), outfile)
     outfile.close()
 
