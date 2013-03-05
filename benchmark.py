a27a80a7474930024d2e894a55206711af5d9d4f
Prabhas Pokharel <prabhas@pokharel@gmail.com>
2013-03-05 10:43:38 -0500

    changing the way benchmarks are run; now, first int is num_iterations

--- benchmark.py
+++ benchmark.py
@@ -135,24 +135,26 @@ def time_to_add_5_update_batch(dataset):
 @print_that_function_is_running
 def time_till_import_is_finished(dataset):
     before = time.time()
-    state = dataset.get_info()['state']
-    while state == 'pending':
+    summary = dataset.get_summary()
+    while 'error' in summary.keys():
         time.sleep(1)
-        state = dataset.get_info()['state']
+        summary = dataset.get_summary()
     after = time.time()
     return after - before
 
 if __name__ == "__main__":
     valid_test_sizes = ['1', '10', '100', '1000', '10000', '100000']
-    test_sizes = sys.argv[1:]
+    num_iterations = int(sys.argv[1])
+    test_sizes = sys.argv[2:]
     invalid_test_size = len(test_sizes) == 0 or len([x for x in test_sizes\
             if x not in valid_test_sizes]) > 1
 
     # choosing between 100, 1000, 10000, 100000
     if invalid_test_size:
-        print """provide size of datasets, like one of:
-                   python benchmark.py 1000
-                   python benchmark.py 100 1000 10000
+        print """provide number_iterations, and size of datasets, like one of:
+                   python benhcmark.py num_iter dataset_size1 dataset_size2 dataset_size3 ..
+                   python benchmark.py 1 1000 # 1 iteration; 1000 row dataset
+                   python benchmark.py 3 100 1000 10000 # 3 iterations; 100, 1000, 10000 row datasets
                 Allowed test sizes are %s
              """ % " ".join(valid_test_sizes)
         sys.exit()
@@ -165,10 +167,13 @@ if __name__ == "__main__":
     except IOError as e:
         with open(benchmarkfile, 'wb') as f: f.write('')
     results = read_from_csv(benchmarkfile)
-    for test_size in test_sizes:
-        test_str = str(test_size)
-        water_file = DIR + test_size + '/water.csv'
-        education_file = DIR + test_size + '/education.csv'
-        new_dict = run_test_suite([water_file])
-        results += new_dict
-        write_to_csv(results, benchmarkfile) # write immediately
+    for i in xrange(1,num_iterations):
+        random.shuffle(test_sizes)
+        for test_size in test_sizes:
+            test_str = str(test_size)
+            water_file = DIR + test_size + '/water.csv'
+            education_file = DIR + test_size + '/education.csv'
+            new_dict = run_test_suite([water_file])
+            results += new_dict
+            write_to_csv(results, benchmarkfile) # write immediately
+            time.sleep(30)
